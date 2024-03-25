d97b525f82b076002a7f128108a06dc26f49f40c
gabrielmesquita7 <xelectrozchannel@gmail.com>
2024-03-25 19:30:11 -0300

    follow and unfollow controls

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -47,12 +47,32 @@ class AppController extends Action
         if ($pesquisarPor != '') {
             $usuario = Container::getModel('Usuario');
             $usuario->__set('nome', $pesquisarPor);
+            $usuario->__set('id', $_SESSION['id']);
             $usuarios = $usuario->getAll();
         }
 
         $this->view->usuarios = $usuarios;
 
         $this->render('quemSeguir');
+    }
+
+    public function acao()
+    {
+        $this->validaAutenticacao();
+
+        $acao = isset ($_GET['acao']) ? $_GET['acao'] : '';
+        $id_usuario_seguindo = isset ($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+
+        $usuario = Container::getModel('Usuario');
+        $usuario->__set('id', $_SESSION['id']);
+
+        if ($acao == 'seguir') {
+            $usuario->seguirUsuario($id_usuario_seguindo);
+        } else if ($acao == 'deixar_de_seguir') {
+            $usuario->deixarSeguirUsuario($id_usuario_seguindo);
+        }
+
+        header('Location: /quem_seguir');
     }
 
     public function validaAutenticacao()
