2899d0c9a563798b4e1b2421cf00e6b8e6b8400d
gabrielmesquita7 <xelectrozchannel@gmail.com>
2024-03-25 09:20:57 -0300

    create timeline

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -10,12 +10,40 @@ class AppController extends Action
 {
     public function timeline()
     {
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set('id_usuario', $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+
+        $this->render('timeline');
+    }
+
+    public function tweet()
+    {
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+        $tweet->__set('tweet', $_POST['tweet']);
+        $tweet->__set('id_usuario', $_SESSION['id']);
+        $tweet->salvar();
+
+        header('Location: /timeline');
+
+    }
+
+    public function validaAutenticacao()
+    {
         session_start();
 
-        if ($_SESSION['id'] == '' || $_SESSION['nome'] == '') {
+        if (!isset ($_SESSION['id']) || $_SESSION['id'] == '' || !isset ($_SESSION['nome']) || $_SESSION['nome'] == '') {
             header('Location: /?login=erro');
         } else {
-            $this->render('timeline');
+
         }
     }
 }
--- App/Route.php
+++ App/Route.php
@@ -46,6 +46,12 @@ class Route extends Bootstrap
 			'action' => 'sair'
 		);
 
+		$routes['tweet'] = array(
+			'route' => '/tweet',
+			'controller' => 'AppController',
+			'action' => 'tweet'
+		);
+
 		$this->setRoutes($routes);
 	}
 
