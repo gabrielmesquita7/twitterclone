e6cc2d22d368b10d45dcf9631782c9a33f59fc3d
gabrielmesquita7 <xelectrozchannel@gmail.com>
2024-04-01 08:19:24 -0300

    show other users tweets in timeline

--- App/Models/Tweet.php
+++ App/Models/Tweet.php
@@ -36,12 +36,18 @@ class Tweet extends Model
     {
         $query = "
             select 
-                t.id, t.id_usuario, u.nome, t.tweet, DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data 
+                t.id, 
+                t.id_usuario,
+                u.nome,
+                t.tweet, 
+                DATE_FORMAT(t.data, '%d/%m/%Y %H:%i') as data 
             from 
                 tweets as t
                 left join usuarios as u on (t.id_usuario = u.id)
             where 
                 t.id_usuario = :id_usuario
+                or t.id_usuario in (select id_usuario_seguindo from usuarios_seguidores 
+                where id_usuario = :id_usuario)
             order by
                 t.data desc
         ";
